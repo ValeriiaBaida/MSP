027b4f2215345b4ee8b4aa8d76d1618ae419dd15
Niklas Sander <niklas.san@me.com>
2025-04-27 14:04:50 +0100

    include models in report

--- report/bpmn_to_image.sh
+++ report/bpmn_to_image.sh
@@ -2,15 +2,19 @@
 
 INPUT_DIR="./bpmn_models"
 OUTPUT_DIR="./images/exported_bpmn"
+LIST_FILE="./models_list.tex"
 
 mkdir -p "$OUTPUT_DIR"
 
 npm install -g bpmn-to-image -y
 
+# Clean the old models list
+echo "" > "$LIST_FILE"
+
 for file in "$INPUT_DIR"/*.bpmn; do
   filename=$(basename -- "$file")
   filename_no_ext="${filename%.*}"
-  output_file="$OUTPUT_DIR/$filename_no_ext.svg"
+  output_file="$OUTPUT_DIR/$filename_no_ext.pdf"
 
   if [ ! -e "$output_file" ] || [ "$file" -nt "$output_file" ]; then
     npx bpmn-to-image "$file":"$output_file"
@@ -18,4 +22,7 @@ for file in "$INPUT_DIR"/*.bpmn; do
   else
     echo "Skipped $file (up to date)"
   fi
+
+  # Add entry to models list
+  echo "\\modelentry{$filename_no_ext}" >> "$LIST_FILE"
 done
